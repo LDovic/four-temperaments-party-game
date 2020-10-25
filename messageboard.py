26df877859adc67f6b82530b2d7ed085bbbcd7e3
Ludovic Noble <ludovicnoble@Ludovics-MBP.lan>
2020-10-25 19:08:26 +0000

    minor edit

--- messageboard.py
+++ messageboard.py
@@ -10,13 +10,13 @@ This class creates a message board object.
 class MessageBoard:
     def __init__(self):
         self.buttons = []
-        self.button_y_limit = SCREEN_HEIGHT - 25 - (20 * 4)
+        self.button_y_limit = SCREEN_HEIGHT - 25 - (20 * 1)
         self.base_y_position = SCREEN_HEIGHT - 25
         self.good_party_list = ["This party is rockin'!", "Everyone's having a good time!", "This party is going well!", "The neighbours are complaining about the noise!", "God has blessed this party!", "There are some seriously weird vibes going around!", "People are letting loose and being themselves!", "People have forgotten that they have to go to work on Monday!"]
 
     def scroll_buttons(self):
         for button in list(reversed(self.buttons)):
-            if button.rect.y == 695:
+            if button.rect.y == self.button_y_limit:
                 self.buttons.pop(0)
             button.change_position_xy(button.rect.x, button.rect.y - 20)
 
@@ -24,7 +24,7 @@ class MessageBoard:
         if name not in (b.name for b in self.buttons):
             button = Button(text, (20, self.base_y_position), color, BUTTON_FONT_SIZE, name)
             if self.buttons:
-                if len(self.buttons) > 4:
+                if len(self.buttons) > 1:
                     self.buttons.pop(0)
                 self.scroll_buttons()
             self.buttons.append(button)        
